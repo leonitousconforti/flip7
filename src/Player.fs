367e208423d679d55d75e3775fbecbb2ea1b8d1d
Leo Conforti <leo@leoconforti.us>
2026-06-19 11:17:11 -0500

    implement player

--- src/Player.fs
+++ src/Player.fs
@@ -1,28 +1,20 @@
 namespace Flip7
 
-module Strategy =
-    /// <summary>
-    /// To hit or to stand.
-    /// </summary>
-    type public HitOrStand =
-        | Hit
-        | Stand
+module public Player =
+    type public Player = {
+        Name: string
+        Strategy: Strategy
+        FirmScore: uint
+        Hand: Hand
+    } with
 
-type public Strategy = uint -> Player -> Player list -> (Deck * Deck) -> Strategy.HitOrStand
-and public Player = {
-    Name: string
-    Strategy: Strategy
-    FirmScore: uint
-    Hand: Hand
-} with
+        static member Make(name: string, strategy: Strategy, ?firmScore: uint, ?hand: Hand) : Player =
+            let firmScore = defaultArg firmScore 0u
+            let hand = defaultArg hand []
 
-    static member Make(name: string, strategy: Strategy, ?firmScore: uint, ?hand: Hand) : Player =
-        let hand = defaultArg hand []
-        let firmScore = defaultArg firmScore 0u
-
-        {
-            Name = name
-            Strategy = strategy
-            FirmScore = firmScore
-            Hand = hand
-        }
+            {
+                Name = name
+                Strategy = strategy
+                FirmScore = firmScore
+                Hand = hand
+            }
