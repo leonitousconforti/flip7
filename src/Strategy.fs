cd0a9dddfbb31a20c5d9f7fea4f9833880c4a5e4
Leo Conforti <leo@leoconforti.us>
2026-06-19 11:16:12 -0500

    implement strategy again

--- src/Strategy.fs
+++ src/Strategy.fs
@@ -1,6 +1,27 @@
 namespace Flip7
 
-module Strategy =
+/// <summary>
+/// A strategy is a function that takes the current session number, the
+/// player, the list of other players, and the decks, and returns whether to
+/// hit or stand.
+/// </summary>
+type public Strategy =
+    unit -> Strategy.StrategyPlayer -> Strategy.StrategyPlayer list -> (Deck * Deck) -> Strategy.HitOrStand
+
+module public Strategy =
+    /// <summary>
+    /// A simplified player type for strategies, containing only the information
+    /// that strategies need to make decisions.
+    /// </summary>
+    type public StrategyPlayer = { Name: string; FirmScore: uint; Hand: Hand }
+
+    /// <summary>
+    /// To hit or to stand.
+    /// </summary>
+    type public HitOrStand =
+        | Hit
+        | Stand
+
     /// <summary>
     /// A strategy that always hits.
     /// </summary>
@@ -14,18 +35,7 @@ module Strategy =
         fun _session _player _otherPlayers _decks -> Strategy.Stand
 
     /// <summary>
-    /// A strategy that randomly decides to hit or stand with equal probability.
-    /// </summary>
-    let public Random: Strategy =
-        let random = System.Random()
-        fun _session _player _otherPlayers _decks ->
-            if random.NextDouble() > 0.5 then
-                Strategy.Hit
-            else
-                Strategy.Stand
-
-    /// <summary>
-    /// A strategy that randomly decides to hit or stand with given probability.
+    /// A strategy that randomly hits or stands with a given probability.
     /// </summary>
     let public RandomWithProbability: float -> Strategy =
         let random = System.Random()
@@ -36,8 +46,13 @@ module Strategy =
                 Strategy.Stand
 
     /// <summary>
-    /// A strategy that hits until the hand's score reaches a certain threshold,
-    /// then stands.
+    /// A strategy that randomly hits or stands with a 50% probability.
+    /// </summary>
+    let public Random: Strategy = RandomWithProbability 0.5
+
+    /// <summary>
+    /// A strategy that hits until the hand's score is at least the given
+    /// threshold, then stands.
     /// </summary>
     let public HitUntilScore: uint -> Strategy =
         fun threshold _session player _otherPlayers _decks ->
@@ -56,16 +71,3 @@ module Strategy =
                 Strategy.Hit
             else
                 Strategy.Stand
-
-    /// <summary>
-    /// A strategy that prompts the user for input to decide whether to hit or
-    /// stand.
-    /// </summary>
-    let rec public Prompt: Strategy =
-        fun _session _player _otherPlayers _decks ->
-            match System.Console.ReadLine() with
-            | "h" -> Strategy.Hit
-            | "hit" -> Strategy.Hit
-            | "s" -> Strategy.Stand
-            | "stand" -> Strategy.Stand
-            | _ -> Prompt _session _player _otherPlayers _decks
