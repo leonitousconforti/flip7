17ff9dea71dea0f3d7acd95e83486b99f919e123
Leo Conforti <leo@leoconforti.us>
2026-06-19 11:21:42 -0500

    save

--- src/Player.fs
+++ src/Player.fs
@@ -1,20 +1,19 @@
 namespace Flip7
 
-module public Player =
-    type public Player = {
-        Name: string
-        Strategy: Strategy
-        FirmScore: uint
-        Hand: Hand
-    } with
+type public Player = {
+    Name: string
+    Strategy: Strategy
+    FirmScore: uint
+    Hand: Hand
+} with
 
-        static member Make(name: string, strategy: Strategy, ?firmScore: uint, ?hand: Hand) : Player =
-            let firmScore = defaultArg firmScore 0u
-            let hand = defaultArg hand []
+    static member Make(name: string, strategy: Strategy, ?firmScore: uint, ?hand: Hand) : Player =
+        let firmScore = defaultArg firmScore 0u
+        let hand = defaultArg hand []
 
-            {
-                Name = name
-                Strategy = strategy
-                FirmScore = firmScore
-                Hand = hand
-            }
+        {
+            Name = name
+            Strategy = strategy
+            FirmScore = firmScore
+            Hand = hand
+        }
